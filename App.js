91cc3a06accac45aa45d68d5adf0645a6419ded6
manojkmishra <manojmishra571980@gmail.com>
2018-05-30 09:29:06 +1000

    boiler plate for react native-   yarn add apollo-client@1.9.0-0 react-apollo react react-navigation react-redux redux redux-thunk styled-components  redux-devtools-extension

--- App.js
+++ App.js
@@ -1,21 +1,22 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { UIManager } from 'react-native';
+import { ApolloProvider } from 'react-apollo';
+import { ThemeProvider } from 'styled-components';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+import { store, client } from './src/store';
+import { colors } from './src/utils/constants';
+
+import Welcome from './src/components/Welcome';
+
+if (UIManager.setLayoutAnimationEnabledExperimental) { UIManager.setLayoutAnimationEnabledExperimental(true);}
+
+export default class App extends React.Component 
+{ render() 
+  {  return ( <ApolloProvider store={store} client={client}>
+                <ThemeProvider theme={colors}>
+                  <Welcome />
+                </ThemeProvider>
+              </ApolloProvider>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/components/Welcome.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components/native';
+
+const Root = styled.View` alignItems: center; justifyContent: center;  flex: 1; backgroundColor: ${props => props.theme.WHITE};
+  width: 90%;
+  alignSelf: center;
+`;
+
+const Text = styled.Text` color: ${props => props.theme.PRIMARY}; fontSize: 18; textAlign: center;`;
+
+export default function Welcome() 
+{  return ( <Root>
+                <Text>Front Page of boiler plate!!!</Text>
+            </Root>
+          )
+}
