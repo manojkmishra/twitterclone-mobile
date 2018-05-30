ab99123c653cf28e754e7434be5adfc6d0ef1fc8
manojkmishra <manojmishra571980@gmail.com>
2018-05-30 14:39:10 +1000

    one tweet place created with avatar container place

--- src/components/FeedCard/FeedCard.js
+++ src/components/FeedCard/FeedCard.js
@@ -3,19 +3,20 @@ import styled from 'styled-components/native';
 import FeedCardHeader from './FeedCardHeader';
 import FeedCardBottom from './FeedCardBottom';
 
-const Root = styled.View` minHeight: 180; backgroundColor: red; width: 100%; shadowColor: ${props => props.theme.SECONDARY}; shadowOffset: 0px 2px;
-  shadowRadius: 2; shadowOpacity: 0.1;
-  `;
-
-const CardContentContainer = styled.View `
-  flex: 1;
-  backgroundColor: yellow;
-`;
+const Root = styled.View ` minHeight: 180; backgroundColor: ${props => props.theme.WHITE}; width: 100%; 
+                           shadowColor: ${props => props.theme.SECONDARY}; shadowOffset: 0px 2px;
+                           shadowRadius: 2; shadowOpacity: 0.1;
+                         `;
+const CardContentContainer = styled.View `flex: 1;  padding: 10px 20px 10px 5px;`;
+const CardContentText = styled.Text `fontSize: 14;   textAlign: left; fontWeight: 500; color: ${props => props.theme.SECONDARY};`;
+const text = 'text in CardContentText' ;
 
 function FeedCard()
 {   return (<Root>
              <FeedCardHeader/>
-             < CardContentContainer/>
+             < CardContentContainer >
+                <CardContentText> {text}</CardContentText>
+             </CardContentContainer>
              < FeedCardBottom/>
              </Root>
             );
--- src/components/FeedCard/FeedCardHeader.js
+++ src/components/FeedCard/FeedCardHeader.js
@@ -2,10 +2,15 @@ import React from 'react';
 import styled from 'styled-components/native';
 
 const Root = styled.View ` height: 50; flexDirection: row; alignItems: center; backgroundColor: blue;`;
+const AvatarContainer = styled.View `flex: 0.2; backgroundColor: red; alignSelf: stretch;`;
+const MetaContainer = styled.View ` flex: 1; alignSelf: stretch;`;
 
 function FeedCardHeader() 
 { return (
-          <Root/>
+          <Root>
+            <AvatarContainer/>
+            < MetaContainer />
+          </Root>
         );
 }
 
