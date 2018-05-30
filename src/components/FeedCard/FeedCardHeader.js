36bff1da78a638ed7a0f7f43d5e9b28242530425
manojkmishra <manojmishra571980@gmail.com>
2018-05-30 14:54:12 +1000

    avatar -linkedin image showing on facecardheader.js

--- src/components/FeedCard/FeedCardHeader.js
+++ src/components/FeedCard/FeedCardHeader.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import styled from 'styled-components/native';
+import { fakeAvatar} from '../../utils/constants';
+
+const AVATAR_SIZE = 40; const AVATAR_RADIUS = AVATAR_SIZE / 2;
 
 const Root = styled.View ` height: 50; flexDirection: row; alignItems: center; backgroundColor: blue;`;
-const AvatarContainer = styled.View `flex: 0.2; backgroundColor: red; alignSelf: stretch;`;
+const AvatarContainer = styled.View `flex: 0.2; backgroundColor: red; justifyContent: center; alignSelf: stretch;`;
 const MetaContainer = styled.View ` flex: 1; alignSelf: stretch;`;
+const Avatar = styled.Image `height: ${AVATAR_SIZE}; width: ${AVATAR_SIZE}; borderRadius: ${AVATAR_RADIUS};`;
 
 function FeedCardHeader() 
 { return (
           <Root>
-            <AvatarContainer/>
+            <AvatarContainer><Avatar source={{uri: fakeAvatar }}/></AvatarContainer>
             < MetaContainer />
           </Root>
         );
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,5 +3,7 @@ export const colors = {
   SECONDARY: '#444B52',
   WHITE: '#FFFFFF',
   LIGHT_GRAY: '#CAD0D6',
+
 };
 
+export const fakeAvatar='https://media.licdn.com/dms/image/C4D03AQFonWJd0W7czA/profile-displayphoto-shrink_100_100/0?e=1533168000&v=beta&t=MVYOH4Ux9svGRkzIDvNHhoPSFJFM1eOnwjw4rA9lx6g';
