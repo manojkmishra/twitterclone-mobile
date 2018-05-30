7904f7d2cecde923a4013240cdb9890e4f0be965
manojkmishra <manojmishra571980@gmail.com>
2018-05-30 13:49:36 +1000

    header and bottom dummy added

--- src/components/FeedCard/FeedCard.js
+++ src/components/FeedCard/FeedCard.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import styled from 'styled-components/native';
 import FeedCardHeader from './FeedCardHeader';
+import FeedCardBottom from './FeedCardBottom';
 
-const Root = styled.View`
-  minHeight: 180;
-  backgroundColor: red;
-  width: 100%;
-  shadowColor: ${props => props.theme.SECONDARY};
-  shadowOffset: 0px 2px;
-  shadowRadius: 2;
-  shadowOpacity: 0.1;
+const Root = styled.View` minHeight: 180; backgroundColor: red; width: 100%; shadowColor: ${props => props.theme.SECONDARY}; shadowOffset: 0px 2px;
+  shadowRadius: 2; shadowOpacity: 0.1;
+  `;
+
+const CardContentContainer = styled.View `
+  flex: 1;
+  backgroundColor: yellow;
 `;
 
 function FeedCard()
 {   return (<Root>
              <FeedCardHeader/>
+             < CardContentContainer/>
+             < FeedCardBottom/>
              </Root>
             );
     
---
+++ src/components/FeedCard/FeedCardBottom.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components/native';
+
+const Root = styled.View`height: 40; backgroundColor: pink;`;
+function FeedCardBottom() 
+{  return (  <Root/>
+          );
+}
+
+export default FeedCardBottom;
