c9916867ff30803be48edc0946098bbe07a690f6
manojkmishra <manojmishra571980@gmail.com>
2018-06-01 15:11:22 +1000

    navigation and screens done(all 4)

--- src/navigations.js
+++ src/navigations.js
@@ -1,11 +1,49 @@
 import React, { Component } from 'react';
 import {  addNavigationHelpers,  StackNavigator,  TabNavigator,} from 'react-navigation';
 import { connect } from 'react-redux';
-
 import HomeScreen from './screens/HomeScreen';
+import { colors } from './utils/constants';
+import { FontAwesome, SimpleLineIcons, EvilIcons } from '@expo/vector-icons';
+import ExploreScreen from './screens/ExploreScreen';
+import NotificationsScreen from './screens/NotificationsScreen';
+import ProfileScreen from './screens/ProfileScreen';
+const TAB_ICON_SIZE = 20;
+const Tabs = TabNavigator(
+  { Home1: {  screen: HomeScreen,    
+               navigationOptions: () => ({ headerTitle: 'Home1Title', tabBarIcon: ({ tintColor }) =>
+                                            <FontAwesome size={TAB_ICON_SIZE} color={tintColor} name="home" />,
+                                         }),
+            },
+    Explore: { screen: ExploreScreen, navigationOptions: () => ({ headerTitle: 'Explore', tabBarIcon: ({ tintColor }) =>
+                                                     <FontAwesome size={TAB_ICON_SIZE} color={tintColor} name="search" />,
+                                                         }),
+             },
+    Notifications: { screen: NotificationsScreen, navigationOptions: () => ({  headerTitle: 'Notifications',
+                                                   tabBarIcon: ({ tintColor }) => <FontAwesome size={TAB_ICON_SIZE} color={tintColor} name="bell" />,
+                                                   }),
+                  },
+     Profile: {screen: ProfileScreen, navigationOptions: () => ({ headerTitle: 'Profile',
+                                                 tabBarIcon: ({ tintColor }) =><FontAwesome size={TAB_ICON_SIZE} color={tintColor} name="user" />,
+                                                           }),
+              },
+  },
+  { lazy: true, tabBarPosition: 'bottom', swipeEnabled: false,
+    tabBarOptions: { showIcon: true, showLabel: false, activeTintColor: colors.PRIMARY, inactiveTintColor: colors.LIGHT_GRAY,
+                     style: { backgroundColor: colors.YELLOW, height: 50, paddingVertical: 5,},
+                   },
+  },
+);
+
 const AppMainNav = StackNavigator(
-  {  Home: { screen :  HomeScreen}
-  });
+  {  Home: { screen :  Tabs}
+  },
+  { cardStyle: { backgroundColor: '#F1F6FA', },
+    navigationOptions: () => (
+      { headerStyle: { backgroundColor: 'pink', },
+        headerTitleStyle: { fontWeight: 'bold', color: colors.SECONDARY, },
+      }),
+  }, 
+);
 
 class AppNavigator extends Component 
 {  render() {  const nav = addNavigationHelpers(
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -5,7 +5,7 @@ import FeedCard from '../components/FeedCard/FeedCard';
 const Root = styled.View`
   flex: 1;
   paddingTop: 5;
-  backgroundColor: #f2f2f2
+ backgroundColor: #f2f2f2
 `;
 //const T = styled.Text``;
 const List = styled.ScrollView``;
