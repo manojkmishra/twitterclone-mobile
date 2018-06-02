f9fa33314e3090c583ae310f7f41baa9287b2638
manojkmishra <manojmishra571980@gmail.com>
2018-06-02 18:24:07 +1000

    onPress- going to LoginForm----from authenticationscreen

---
+++ src/components/SignupForm.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import styled from 'styled-components/native';
+
+const Root = styled.View``;
+
+const T = styled.Text``
+
+class SignupForm extends Component
+{ state = {  }
+  render() 
+  {  return (  <Root>
+                   <T>SignupForm</T>
+               </Root>
+           );
+  }
+}
+export default SignupForm;
