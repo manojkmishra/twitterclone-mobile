ffa57611e98d9cad48595a94b4af2bf8d63b9a89
manojkmishra <manojmishra571980@gmail.com>
2018-06-03 17:24:01 +1000

    redux-logger installed and enabled in store

--- src/store.js
+++ src/store.js
@@ -2,6 +2,7 @@ import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import ApolloClient, { createNetworkInterface } from 'apollo-client';
 import thunk from 'redux-thunk';
+import { createLogger } from 'redux-logger';
 
 import reducers from './reducers';
 
@@ -10,6 +11,7 @@ const networkInterface = createNetworkInterface({  uri: 'http://192.168.0.3:3000
 
 export const client = new ApolloClient({  networkInterface,});
 
-const middlewares = [client.middleware(), thunk];
+//const middlewares = [client.middleware(), thunk];
+const middlewares = [client.middleware(), thunk, createLogger()];
 
 export const store = createStore( reducers(client), undefined, composeWithDevTools(applyMiddleware(...middlewares)),);
