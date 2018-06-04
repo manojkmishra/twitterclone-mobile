4026940ae244f3ba796a8cb6701e64d8de011af3
manojkmishra <manojmishra571980@gmail.com>
2018-06-04 16:30:55 +1000

    front page--->goes to Tweets---no apploading

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import { UIManager } from 'react-native';
+import { UIManager, AsyncStorage } from 'react-native';
 import { ApolloProvider } from 'react-apollo';
 import { ThemeProvider } from 'styled-components';
 
 import { store, client } from './src/store';
 import { colors } from './src/utils/constants';
+import { login } from './src/actions/user';
+import { AppLoading } from 'expo';
 
 //import Welcome from './src/components/Welcome';
 //import HomeScreen from './src/screens/HomeScreen';
@@ -13,8 +15,17 @@ import AppNavigation from './src/navigations';
 if (UIManager.setLayoutAnimationEnabledExperimental) { UIManager.setLayoutAnimationEnabledExperimental(true);}
 
 export default class App extends React.Component 
-{ render() 
-  {  return ( <ApolloProvider store={store} client={client}>
+{ //state = { appIsReady: false, };
+  componentWillMount() {   this._checkIfToken();  }
+  _checkIfToken = async () => 
+  { try { const token = await AsyncStorage.getItem('@twitteryoutubeclone');
+          if (token != null) {  store.dispatch(login());  }
+        } catch (error) {  throw error; }
+   // this.setState({ appIsReady: true });
+  };
+  render() 
+  { //if (!this.state.appIsReady) { return <AppLoading />; } 
+    return ( <ApolloProvider store={store} client={client}>
                 <ThemeProvider theme={colors}>
                   < AppNavigation store={store} />
                 </ThemeProvider>
