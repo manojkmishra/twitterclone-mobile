7a989e9ff9aa4b1fac269c277c6617dfcc2bc8e8
manojkmishra <manojmishra571980@gmail.com>
2018-06-04 16:54:00 +1000

    middleware added(http header- authorization= bearer token)

--- src/store.js
+++ src/store.js
@@ -3,11 +3,21 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import ApolloClient, { createNetworkInterface } from 'apollo-client';
 import thunk from 'redux-thunk';
 import { createLogger } from 'redux-logger';
-
 import reducers from './reducers';
-
+import { AsyncStorage } from 'react-native';
 //const networkInterface = createNetworkInterface({  uri: 'http://localhost:3000/graphiql',});
 const networkInterface = createNetworkInterface({  uri: 'http://192.168.0.2:3000/graphql',});
+
+networkInterface.use(
+[{  async applyMiddleware(req, next) 
+    { if (!req.options.headers) {      req.options.headers = {};    }
+      try {  const token = await AsyncStorage.getItem('@twitteryoutubeclone');
+             if (token != null) 
+             {  req.options.headers.authorization = `Bearer ${token}` || null;  }
+          } catch (error) { throw error; }
+      return next();
+    }
+}])
 
 export const client = new ApolloClient({  networkInterface,});
 
