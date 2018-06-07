30d1ba6d85462656f9b6d3ddb11302a9082e39df
manojkmishra <manojmishra571980@gmail.com>
2018-06-07 18:21:25 +1000

    logout done- async jwt deleted- not logging in after refresh: so all good

--- src/actions/user.js
+++ src/actions/user.js
@@ -1,3 +1,4 @@
+import { AsyncStorage } from 'react-native';
 export function login() 
 { console.log('/src/actions/user.js-type:LOGIN')
   return { type: 'LOGIN' }
@@ -12,5 +13,10 @@ export function getUserInfo(info)
 
 export function logout() 
 { console.log('/src/actions/user.js-type:LOGOUT')
-  return { type: 'LOGOUT' }
+ // return { type: 'LOGOUT' }
+ return async (dispatch) => 
+  { try {  await AsyncStorage.removeItem('@twitteryoutubeclone');
+          return dispatch({  type: 'LOGOUT'})
+       } catch (error) {  throw error; }
+  }
 }
