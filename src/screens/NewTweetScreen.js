b87e9320f85820ba6caccd204013cdbeed17e80b
manojkmishra <manojmishra571980@gmail.com>
2018-06-09 11:38:12 +1000

    tweet button created below the tweet write place--backgnd color of pink removed

--- src/screens/NewTweetScreen.js
+++ src/screens/NewTweetScreen.js
@@ -2,14 +2,20 @@ import React, { Component } from 'react';
 import styled from 'styled-components/native';
 import { colors } from '../utils/constants';
 import { Platform, Keyboard } from 'react-native';
+import Touchable from '@appandflow/touchable';
 
 const Root = styled.View` backgroundColor: ${props => props.theme.WHITE}; flex: 1; alignItems: center;`;
-const Wrapper = styled.View` height: 80%; width: 90%; paddingTop: 5; backgroundColor: pink; `;
+const Wrapper = styled.View` height: 80%; width: 90%; paddingTop: 5;   position: relative; `;
 const Input = styled.TextInput.attrs({ multiline: true, placeholder: "Write Your Tweet", maxLength: 140,
               selectionColor: Platform.OS === 'ios' && colors.PRIMARY, autoFocus: true,})`  
               height: 40%;  width: 100%;  fontSize: 18;  color: ${props => props.theme.SECONDARY};`;
 //const T = styled.Text``
 //autofocus in const Input---for keyboard to appear as we come to new tweet page
+const TweetButton = styled(Touchable).attrs({ feedback: 'opacity', hitSlop: { top: 20, left: 20, right: 20, bottom: 20 },})`
+                    backgroundColor: ${props => props.theme.PRIMARY}; justifyContent: center; alignItems: center; width: 80; height: 40;
+                    borderRadius: 20; position: absolute; top: 60%; right: 0;`;
+//--position: absolute; top: 60%; right: 0---will keep the button on right --down of 60% place
+const TweetButtonText = styled.Text` color: ${props => props.theme.WHITE}; fontSize: 16;`;
 
 class NewTweetScreen extends Component
 { state = { text: '', };
@@ -18,6 +24,9 @@ class NewTweetScreen extends Component
   {  return ( <Root>
                <Wrapper>
                  <Input value={this.state.text} onChangeText={this._onChangeText} />
+                 <TweetButton>
+                    <TweetButtonText>Tweet</TweetButtonText>
+                 </TweetButton>
                </Wrapper>
               </Root>
             );
