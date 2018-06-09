773c08cc67afa5d0c5ec718f33989e34e5e1b61c
manojkmishra <manojmishra571980@gmail.com>
2018-06-09 11:13:16 +1000

    able to write tweet on newtweetpage---keybaord opens when we go to this page

--- src/screens/NewTweetScreen.js
+++ src/screens/NewTweetScreen.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
 import styled from 'styled-components/native';
+import { colors } from '../utils/constants';
+import { Platform, Keyboard } from 'react-native';
 
 const Root = styled.View` backgroundColor: ${props => props.theme.WHITE}; flex: 1; alignItems: center;`;
-const Wrapper = styled.View` height: 80%; width: 90%; paddingTop: 5; backgroundColor: red; `;
+const Wrapper = styled.View` height: 80%; width: 90%; paddingTop: 5; backgroundColor: pink; `;
+const Input = styled.TextInput.attrs({ multiline: true, placeholder: "Write Your Tweet", maxLength: 140,
+              selectionColor: Platform.OS === 'ios' && colors.PRIMARY, autoFocus: true,})`  
+              height: 40%;  width: 100%;  fontSize: 18;  color: ${props => props.theme.SECONDARY};`;
 const T = styled.Text``
+//autofocus in const Input---for keyboard to appear as we come to new tweet page
 
 class NewTweetScreen extends Component {
   state = {  }
@@ -11,7 +17,7 @@ class NewTweetScreen extends Component {
     return (
       <Root>
         <Wrapper>
-        <T>NewTweetScreen</T>
+        <Input/>
         </Wrapper>
       </Root>
     );
