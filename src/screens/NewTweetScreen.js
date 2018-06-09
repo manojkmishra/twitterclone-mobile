582cba605c8caf2c46bf35b4e9a64b439b2b0a34
manojkmishra <manojmishra571980@gmail.com>
2018-06-09 10:33:47 +1000

    newtwwetscreen- wrapper added of red color

--- src/screens/NewTweetScreen.js
+++ src/screens/NewTweetScreen.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import styled from 'styled-components/native';
 
-const Root = styled.View``;
-
+const Root = styled.View` backgroundColor: ${props => props.theme.WHITE}; flex: 1; alignItems: center;`;
+const Wrapper = styled.View` height: 80%; width: 90%; paddingTop: 5; backgroundColor: red; `;
 const T = styled.Text``
 
 class NewTweetScreen extends Component {
@@ -10,7 +10,9 @@ class NewTweetScreen extends Component {
   render() {
     return (
       <Root>
+        <Wrapper>
         <T>NewTweetScreen</T>
+        </Wrapper>
       </Root>
     );
   }
